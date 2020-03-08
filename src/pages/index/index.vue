783353f8e2207753b3038c972a66f0cca6c65f5f
凉飕飕 <739092064@qq.com>
2020-03-08 20:51:55 +0800

    '楼层商品的实现'

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -36,7 +36,32 @@
 			<image src="/static/uploads/icon_index_nav_4@2x.png"/>
 		</navigator>
 	</view>
-	
+	<!-- // 楼层商品 -->
+	<view class="floors">
+		<view class="floor">
+			<!-- 标题 -->
+			<view class="title">
+				<image src="/static/uploads/pic_floor01_title.png"/>
+			</view>
+			<!-- 条目 -->
+			<view class="items">
+				<navigator url="">
+					<image src="/static/uploads/pic_floor01_1@2x.png"/>
+				</navigator>
+				<navigator url="">
+					<image src="/static/uploads/pic_floor01_2@2x.png"/>
+				</navigator>
+				<navigator url="">
+					<image src="/static/uploads/pic_floor01_3@2x.png"/>
+				</navigator>
+				<navigator url="">
+					<image src="/static/uploads/pic_floor01_4@2x.png"/>
+				</navigator>
+				<navigator url="">
+					<image src="/static/uploads/pic_floor01_5@2x.png"/>
+				</navigator>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -89,5 +114,51 @@
 			}
 		}
 	}
-	
+	.floor{
+		.title{
+			width: 750rpx;
+			height: 60rpx;
+			padding: 20rpx 0 0 10rpx;
+			box-sizing: border-box;
+			background-color: #F4F4F4;
+		}
+		.items{
+			padding: 20rpx 16rpx;
+			overflow: hidden;
+			navigator{
+				float: left;
+				padding-left: 10rpx;
+				&:first-child{
+					width: 232rpx;
+					height: 386rpx;
+					padding-left: 0;
+				}
+				&:nth-child(2),
+				&:nth-child(5){
+					width: 273rpx;
+					height: 188rpx;
+				}
+				&:nth-child(3),
+				&:nth-child(4){
+					width: 193rpx;
+					height: 188rpx;
+				}
+				&:nth-last-child(-n+2){
+					padding-top: 10rpx;
+				}
+			}
+		}
+		&:first-child{
+			.items{
+				navigator{
+					width: 232rpx;
+					height: 188rpx;
+					&:first-child{
+						width: 232rpx;
+						height: 386rpx;
+					}
+				}
+			}
+		}
+	}
 </style>
